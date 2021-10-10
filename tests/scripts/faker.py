3e9b29ed568c26b764ff9fe830b56a50ec271d4d
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-10 18:19:56 +0330

    provide better faker and match with cmake

--- tests/scripts/faker.py
+++ tests/scripts/faker.py
@@ -2,30 +2,48 @@ import sqlite3
 import pathlib
 
 class Queries():
-    config = '''CREATE TABLE config (
-	  id INTEGER PRIMARY KEY AUTOINCREMENT,
-	  mask TEXT NOT NULL,
-	  router TEXT NOT NULL,
-	  domain TEXT NOT NULL,
-	  lease_time INTEGER NOT NULL DEFAULT 600
-    );'''
+    def __init__(self) -> None:
+        self.tables = {
+            "config" : "config",
+            "pool" : "pool"
+        }
 
-    pool = '''CREATE TABLE pool ( 
-	   id INTEGER PRIMARY KEY AUTOINCREMENT,
-	   conf_id INTEGER NOT NULL,
-	   ip TEXT NOT NULL,
-	   host TEXT NOT NULL,
-	   mac TEXT NOT NULL,
-	   lease_time INTEGER NOT NULL DEFAULT 600,
-	   FOREIGN KEY (conf_id) REFERENCES config(id)
-    );'''
+    @property
+    def config(self):
+        return '''CREATE TABLE {} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            mask TEXT NOT NULL,
+            router TEXT NOT NULL,
+            domain TEXT NOT NULL,
+            lease_time INTEGER NOT NULL DEFAULT 600
+            );'''.format(self.tables["config"])
+
+    @property
+    def pool(self):
+        return '''CREATE TABLE {} ( 
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            conf_id INTEGER NOT NULL,
+            ip TEXT NOT NULL,
+            host TEXT NOT NULL,
+            mac TEXT NOT NULL,
+            lease_time INTEGER NOT NULL DEFAULT 600,
+            FOREIGN KEY (conf_id) REFERENCES config(id)
+            );'''.format(self.tables["pool"])
+
+    def deleteAll(self, conn: sqlite3.Connection):
+        for table in self.tables:
+            conn.execute("DROP TABLE IF EXISTS {};".format(table))
 
 currentPath = str(pathlib.Path(__file__).parent.resolve())
 
-dhcpdConn = sqlite3.connect(currentPath + "/dhcpd.db")
+dhcpdConn = sqlite3.connect(currentPath + "/../database/dhcpd.db")
 
-dhcpdConn.execute(Queries.config)
+qr = Queries()
 
-dhcpdConn.execute(Queries.pool)
+qr.deleteAll(dhcpdConn)
+
+dhcpdConn.execute(qr.config)
+
+dhcpdConn.execute(qr.pool)
 
 dhcpdConn.commit()

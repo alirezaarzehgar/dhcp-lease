620f4c91eb0998649ea3b2bb49abbf663658e1ea
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-10 16:20:36 +0330

    basic structure

---
+++ tests/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author alirezaarzehgar (alirezaarzehgar82@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "main.h"
---
+++ tests/main.h
@@ -0,0 +1,6 @@
+#if !defined(LEASE_TESTS_MAIN_H)
+#define LEASE_TESTS_MAIN_H
+
+
+
+#endif // LEASE_TESTS_MAIN_H

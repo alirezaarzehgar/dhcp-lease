148020eeeb454eb29304318e242ce2865f31dee6
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-11 10:04:18 +0330

    complete faker

--- tests/scripts/faker.py
+++ tests/scripts/faker.py
@@ -1,5 +1,7 @@
+from abc import abstractclassmethod, abstractmethod
 import sqlite3
 import pathlib
+
 
 class Queries():
     def __init__(self) -> None:
@@ -26,7 +28,7 @@ class Queries():
             ip TEXT NOT NULL,
             host TEXT NOT NULL,
             mac TEXT NOT NULL,
-            lease_time INTEGER NOT NULL DEFAULT 600,
+            lease_flag INTEGER NOT NULL DEFAULT 0,
             FOREIGN KEY (conf_id) REFERENCES config(id)
             );'''.format(self.tables["pool"])
 
@@ -34,16 +36,80 @@ class Queries():
         for table in self.tables:
             conn.execute("DROP TABLE IF EXISTS {};".format(table))
 
+    def insertToConf(self, mask: str, router: str, domain: str, lease_time: int) -> None:
+        return '''INSERT INTO {} (
+            mask,
+            router,
+            domain,
+            lease_time
+        ) VALUES (
+            "{}", "{}", "{}", {}
+        );
+        '''.format(self.tables["config"], mask, router, domain, lease_time)
+
+    def insertToPool(self, config_id: int, ip: str, host: str, mac: str, lease_flag: bool) -> None:
+        return '''INSERT INTO {} (
+            conf_id,
+            ip,
+            host,
+            mac,
+            lease_flag
+        ) VALUES (
+            {}, "{}", "{}", "{}", {}
+        );
+        '''.format(self.tables["pool"], config_id, ip, host, mac, lease_flag)
+
+class Faker():
+    def __init__(self, conn: sqlite3.Connection, quiery: Queries) -> None:
+        self.conn = conn
+        self.qr = quiery
+
+    def createTables(self):
+        self.qr.deleteAll(self.conn)
+
+        self.conn.execute(self.qr.config)
+
+        self.conn.execute(self.qr.pool)
+
+        self.conn.commit()
+
+        return self
+
+    def conf(self):
+        for i in range(1, 5):
+            self.conn.execute(
+                self.qr.insertToConf(
+                    "255.255.255." + str(5 * i),
+                    "192.168.1." + str(i * 8),
+                    str(i * 12) + ".example.com",
+                    600
+                )
+            )
+
+        self.conn.commit()
+
+        return self
+
+    def pool(self):
+        for i in range(15):
+            self.conn.execute(
+                self.qr.insertToPool(
+                    (i % 5) + 1,
+                    "192.168.1." + str(i),
+                    "ali" + str(i),
+                    "12:a2:32:08:00:a1",
+                    i % 2
+                )
+            )
+
+        self.conn.commit()
+
+        return self
+
 currentPath = str(pathlib.Path(__file__).parent.resolve())
 
-dhcpdConn = sqlite3.connect(currentPath + "/../database/dhcpd.db")
+dhcpdConn = sqlite3.connect(currentPath + "/dhcpd.db")
 
-qr = Queries()
+faker = Faker(dhcpdConn, Queries())
 
-qr.deleteAll(dhcpdConn)
-
-dhcpdConn.execute(qr.config)
-
-dhcpdConn.execute(qr.pool)
-
-dhcpdConn.commit()
+faker.createTables().conf().pool()
